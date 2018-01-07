6a189d4c2e57d8ce5a2c03571cac74fd0d3425b4
ssi <sergeev.s.i1991@gmail.com>
2018-01-07 08:23:15 +0700

    proxy application

---
+++ app.py
@@ -0,0 +1,43 @@
+import asyncio
+
+from aiohttp import web, ClientSession
+
+from rate_limit import RLQ, rate_limit_middleware
+import argparse
+
+BASE_URL_TEMPLATE = "http://127.0.0.1:8001/{}"
+
+
+async def _response(method, path, params, body):
+    session = ClientSession()
+    response = await session.request(method, BASE_URL_TEMPLATE.format(path), params=params, data=body)
+    session.close()
+    return response
+
+
+async def index_view(request):
+    response = await _response(request.method, request.match_info['path'], request.query, await request.read())
+    _headers = dict()
+    for h in response.headers:
+        if h not in ("Content-Encoding", "Transfer-Encoding"):  # exclude hop-by-hop headers
+            _headers[h] = response.headers[h]
+    return web.Response(body=response.content._buffer[0], headers=_headers, status=response.status)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--base-host', required=True)
+    parser.add_argument('-p', '--port', type=int, required=True)
+    args = parser.parse_args()
+
+    BASE_URL_TEMPLATE = args.base_host + "/{}"
+
+    q = RLQ()
+    loop = asyncio.get_event_loop()
+
+    app = web.Application(middlewares=[rate_limit_middleware(q)], debug=True)
+    app.router.add_route('*', '/{path:.*}', index_view)
+
+    print("Run the server", "BASE HOST: {}".format(args.base_host), "PORT: {}".format(args.port), sep='\n')
+    loop.run_until_complete(loop.create_server(app.make_handler(), '127.0.0.1', args.port))
+    loop.run_forever()
